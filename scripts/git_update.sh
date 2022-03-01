2f8fa65673590b329413b60786424e21fd62942c
Diego Coral <diego.corral@walmart.com>
2022-03-01 11:28:05 -0500

    add role: Container Registry Service Agent in GCP ROLE

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -46,7 +46,7 @@ fi
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
-# get current hash and see if it already has a tag
+# get current hash and see if it already has a tag 
 GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
